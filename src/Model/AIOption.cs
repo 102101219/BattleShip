3081cd08c6ff59f418eed78299999ee2ab8837ef
OshanViranga <62125299+OshanViranga@users.noreply.github.com>
2020-04-05 23:05:33 +1000

    Update AIOptions.cs

--- src/Model/AIOption.cs
+++ src/Model/AIOption.cs
@@ -1,19 +1,21 @@
-''' <summary>
-''' The different AI levels.
-''' </summary>
-Public Enum AIOption
-    ''' <summary>
-    ''' Easy, total random shooting
-    ''' </summary>
-    Easy
+/// <summary>
+/// The different AI levels.
+/// </summary>
 
-    ''' <summary>
-    ''' Medium, marks squares around hits
-    ''' </summary>
-    Medium
+public enum AIOption
+{
+    /// <summary>
+    /// Easy, total random shooting
+    /// </summary>
+    Easy,
 
-    ''' <summary>
-    ''' As medium, but removes shots once it misses
-    ''' </summary>
+    /// <summary>
+    /// Medium, marks squares around hits
+    /// </summary>
+    Medium,
+
+    /// <summary>
+    /// As medium, but removes shots once it misses
+    /// </summary>
     Hard
-End Enum
+}
